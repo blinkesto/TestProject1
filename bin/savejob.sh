a943c6c418b9ef621a0c799289c7497a54b05902
Koy Dooley <koydooley@gmail.com>
2019-07-18 09:25:26 -0500

    Update job name

--- bin/savejob.sh
+++ bin/savejob.sh
@@ -13,6 +13,6 @@ JOB=$1
 java -jar $PROJECT_PATH/bin/rundeck-cli-1.1.7-all.jar jobs list  -p TestProject1 --file $PROJECT_PATH/jobs/$JOB.yaml --format yaml --jobxact $JOB
 
 if [ $? -eq 0 ]; then
-    git commit -am "Save $JOB"
+    git commit $PROJECT_PATH/jobs/$JOB.yaml -m "Save $JOB"
     git push
 fi
